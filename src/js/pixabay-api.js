8f975ed659ca60253e7f7a8d74b1e9683c7ad098
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-07-03 10:59:44 +0300

    Summary reqvared

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,1 +1,12 @@
 //1 task
+// export/import:
+
+// У файлі pixabay-api.js зберігай функції для HTTP-запитів.
+// У файлі render-functions.js створи функції для відображення елементів інтерфейсу.
+// У файлі main.js напиши всю логіку роботи додатка.
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
