a8ec2b2b646bc7c28d4186f06a1db7f1c89a10dd
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-07-07 15:21:21 +0000

    Deploying to gh-pages from @ Olexandr-L45D/goit-js-hw-11@d84504a9b198a235dd40c58b4d9d555bf29cd955 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,13 +1,13 @@
-import{i as u,S as f}from"./assets/vendor-0fc460d7.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))t(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const l of o.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&t(l)}).observe(document,{childList:!0,subtree:!0});function a(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function t(e){if(e.ep)return;e.ep=!0;const o=a(e);fetch(e.href,o)}})();const m="44760113-b733d2f51a4c6409aa3483a05",d={webformatURL:"",largeImageURL:"",tags:"alt",views:1,downloads:1,likes:1,comments:1,user_id:1,user:""};function i(s){return fetch(`https://pixabay.com/api/?key=${m}`,d).then(r=>{if(console.log(r),!r.ok)throw new Error(r.status);return r.json()})}const y=document.querySelector(".uzers-form-image");y.addEventListener("submit",h);function h(s){const r=s.currentTarget.elements;r.elements.searchQuery.value.toLowerCase().trim(),i().then(n).catch(p).finally(()=>r.reset())}const c=document.querySelector(".gallery");c.addEventListener("submit",n);function n(s,r){const a=document.parameters;a.map(({image:t})=>` <div class="gallery">"${a}"
+import{S as i}from"./assets/vendor-338edac8.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const l of t.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&n(l)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const u="44760113-b733d2f51a4c6409aa3483a05";function f(){return fetch(`https://pixabay.com/api/?key=${u}&q=cat`).then(o=>{if(!o.ok)throw new Error(o.status);return o.json()})}var m=new i(".gallery a",{captionDelay:200,captionsData:"alt"});const a=document.querySelector(".gallery");a.addEventListener("submit",c);function c(o){const s=o.map(r=>` 
     <li class="gallery-item">
-    <p>${t.views}</p>
-    <p>${t.downloads}</p>
-    <p>${t.likes}</p>
-    <p>${t.comments}</p>
-    <a class="gallery-link" href="${t.webformatURL}">
-    <img class="gallery-image" src="${t.orientat}" alt="${t.tags}" title="${t.name}"/></a>
-    <a class="gallery-link" href="${t.largeImageURL}">
-    <img class="gallery-image" src="${t.orientat}" alt="" title="${t.tags}"/></a>
+    <a class="gallery-link" href="${r.largeImageURL}">
+    <img class="gallery-image" src="${r.webformatURL}" alt="${r.tags}" title="${r.name}"/></a>
+    <div class="gallery-paragraf">
+    <p class="gallery-commant">likes ${r.likes}</p>
+    <p class="gallery-commant">views ${r.views}</p>
+    <p class="gallery-commant">comments ${r.comments}</p>
+    <p class="gallery-commant">downloads ${r.downloads}</p>
+    </div>
     </li> 
-    </div> `).join("").refresh(),c.innerHTML("afterbegin",n())}function p(s){u.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"})}new f(".gallery a",{captionDelay:200,captionsData:"alt"});i().then(s=>console.log(s));n().then(s=>console.log(s));
+     `).join("");a.insertAdjacentHTML("beforeend",s),m.refresh()}const d=document.querySelector(".uzers-form-image");d.addEventListener("submit",p);function p(o){o.preventDefault(),f().then(s=>{console.log(s),c(s.hits)})}
 //# sourceMappingURL=commonHelpers.js.map
