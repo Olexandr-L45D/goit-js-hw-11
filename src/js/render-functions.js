3c123135d8123f2acae59842173eda313c9ee351
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-07-09 12:01:05 +0300

    summ reqw mon 1 at 09/07

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -3,12 +3,24 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+iziToast.settings({
+  timeout: 2500,
+  resetOnHover: true,
+  transitionIn: 'flipInX',
+  transitionOut: 'flipOutX',
+  position: 'topRight',
+  titleSize: 25,
+  messageSize: 25,
+  backgroundColor: 'rgba(255, 182, 66, 0.8)',
+});
 var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 200, captionsData: 'alt'  });
 
 const galleryContainer = document.querySelector('.gallery'); // function створює розмітку для галереї
 galleryContainer.addEventListener('submit', renderGalleryMarkap);
-   
+
    export function renderGalleryMarkap(images) {
+    const element = document.getElementsByClassName(".gallery");
+    element.innerHTML = "";
     const markup = images
     .map((image) =>  
       ` 
@@ -25,29 +37,40 @@ galleryContainer.addEventListener('submit', renderGalleryMarkap);
      ` )
       .join("")
       galleryContainer.insertAdjacentHTML("beforeend", markup);
-      lightbox.refresh()
+      lightbox.refresh();
+      
   };
-// проста функція виклика повідомлення про помилку без перевірки and iziToast
- function fechGallery(error) {
-  iziToast.error({title: 'Error', 
-    message: `Sorry, there are no images matching your search query. Please try again!`})
- }
- export {fechGallery};
 
+  export const refs = {
+    gallery: document.querySelector('.gallery'),
+    loader: document.querySelector('.loader'),
+  };
+// функція виклика повідомлення про помилку and iziToast
 
+export function fechGallery(error) {
+  switch (error) {
+    case 'outdata':
+      iziToast.warning({
+        title: 'Error',
+        message: 'Введіть данні для пошуку!',
+      });
+      break;
+    case 'nodata':
+      iziToast.warning({
+        title: 'Error',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+      break;
+    default:
+      iziToast.error({
+        title: 'Error',
+        message: 'Щось пішло не так. Ми працюемо над вирішенням питання!',
+      });
+      break;
+  }
+}
 
-
-
- 
-
-// підключити ще бібліотеку для завантаження спінера(нижче взяв з репозиторію) СПІНЕР???
-// const main = document.getElementById('main');
-// // Create Spinners 
-// LOADERS.forEach((loader, i) => {    
-//     // Append Loader
-//     main.appendChild(createLoader(i));
-    
-// })
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,26 +1,30 @@
 
-
-
-
 import {getImage} from "./js/pixabay-api"
- import {fechGallery} from "./js/render-functions"
+ import {fechGallery, refs} from "./js/render-functions"
 import {renderGalleryMarkap} from "./js/render-functions"
-
 
 const formSearchImage = document.querySelector(".uzers-form-image");//все запускається з сабміта форми
 formSearchImage.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
     event.preventDefault();
+    const searchText = event.target.searchQuery.value;
+  if (!searchText) {
+    fechGallery('outdata');
+    return;
+  }
+  refs.gallery.innerHTML = '';
+  refs.loader.classList.add('loader');
     const form = event.currentTarget; // посилання на елемент форми
     const photQueryValue = form.elements.searchQuery.value.toLowerCase().trim(); //значення яке написав користувач
 
-    getImage(photQueryValue).then(data => {
-        console.log(data);
+    getImage(photQueryValue)
+    .then(data => {
         renderGalleryMarkap(data.hits)
-    }).catch(error => {console.error(error);
-             fechGallery()
-           })
-    .finally(() => form.reset()); //очистка данних форми після закінчення промісу(очистка тексту в інпуті)
+        
+    }).catch((err) => console.error(err))
+    .finally(() => 
+        form.reset()); //очистка тексту в інпуті
+    
 }
 
